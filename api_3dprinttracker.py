5c664a484db93d2f86ae80d7a90acceeacc65380
Techno-Hwizrdry <alex.mardigian@gmail.com>
2020-01-23 15:58:00 -0500

    Bug fixes and new unit tests.
    Bug fixes
    ---------
    If a general Exception was caught (i.e.: the API can't connect to the database) in any of the endpoints, a 200 HTTP code would be returned.
    What should happen is that the endpoints should return a 500 HTTP code, indicating a internal server error.
    
    Replaced all the abort() calles with statemens that return JSON with a status code.
    The function, create_JSON_response(), was created for this purpose.
    
    New unit tests
    --------------
    Added tests for HTTP 500 responses from the endpoints.
    Added a test for HTTP 404 response for the /file/{filename} endpoint.

--- api_3dprinttracker.py
+++ api_3dprinttracker.py
@@ -8,6 +8,8 @@ from   flask     import abort, jsonify, request
 import sys
 import traceback
 
+MSG_500 = "Internal server error."
+
 START_DATETIME_INDEX = 4
 
 def update_file_data(conn, cursor, filename):
@@ -58,6 +60,12 @@ def create_file_data(data, dtimes):
 
     return file_data
 
+def create_JSON_response(status, message):
+    response = jsonify(message)
+    response.status_code = status
+
+    return response
+
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>3D Print Tracker API</h1>'''
@@ -87,16 +95,17 @@ def get_files():
                 id = row[0]
                 _3dprint = create_file_data(row, datetimes[id])
                 _3dprints.append(_3dprint)
+
+        return jsonify({'file': _3dprints})
     except Exception as e:
         traceback.print_exc(file=sys.stderr)
+        return create_JSON_response(500, MSG_500)
     finally:
         if cursor:
             cursor.close()
 
         if conn:
             conn.close()
-
-    return jsonify({'files': _3dprints})
 
 @app.route('/file/<string:filename>', methods=['GET'])
 def get_file(filename):
@@ -113,18 +122,18 @@ def get_file(filename):
         response = cursor.fetchall()
         file_data = create_file_data(response[0], get_datetimes(response))
 
+        return jsonify({'file': file_data})
     except IndexError:
-        abort(404)  # File not found.
+        return create_JSON_response(404, f"File '{filename}' not found.")
     except Exception as e:
         traceback.print_exc(file=sys.stderr)
+        return create_JSON_response(500, MSG_500)
     finally:
         if cursor:
             cursor.close()
 
         if conn:
             conn.close()
-
-    return jsonify({'file': file_data})
 
 @app.route('/file/latestprint', methods=['GET'])
 def get_latest_print():
@@ -140,18 +149,19 @@ def get_latest_print():
         cursor.execute(sql)
         response  = cursor.fetchall()[0]
         file_data = create_file_data(response, response[START_DATETIME_INDEX])
+
+        return jsonify({'file': file_data})
     except IndexError:
-        pass   # There are no records in the 3dprints table. An empty JSON object will be returned. 
+        return jsonify({'file': file_data})   # There are no records in the 3dprints table. An empty JSON object will be returned. 
     except Exception as e:
         traceback.print_exc(file=sys.stderr)
+        return create_JSON_response(500, MSG_500)
     finally:
         if cursor:
             cursor.close()
 
         if conn:
             conn.close()
-
-    return jsonify({'file': file_data})
 
 # To successfully add a new record, the PUT request needs to supply a 'filename' and a 'lastPrintTime'.
 # lastPrintTime is in seconds.
@@ -161,7 +171,7 @@ def put_file():
     # If there is no filename in the json PUT request,
     # or no json at all, then return a 400 (Bad Request) HTTP error code.
     if not request.json or not 'filename' in request.json:
-        abort(400)
+        return create_JSON_response(400, "Bad request.")
 
     SECONDS_IN_HOUR = 3600
     DECIMAL_PLACES  = 2
@@ -183,11 +193,9 @@ def put_file():
         # the print count by 1.  Otherwise, create a new record..
         if len(filename_data) > 0:
             update_file_data(conn, cursor, filename)
-            response = jsonify(f"{filename} updated successfully.")
-            response.status_code = 200
+            response = create_JSON_response(200, f"{filename} updated successfully.")
         elif not 'lastPrintTime' in request.json:    # 'lastPrintTime' is needed to create a new record in 3dprints.
-            response = jsonify(f"ERROR: 'lastPrintTime' was not provided in request.json.")
-            response.status_code = 400
+            return create_JSON_response(400, "Bed request. 'lastPrintTime' was not provided in request.json.")
         else:
             print_hours = round(request.json['lastPrintTime'] / SECONDS_IN_HOUR, DECIMAL_PLACES)
             sql  = "INSERT INTO 3dprints(filename, print_count, print_time) VALUES(%s, %s, %s)"
@@ -196,20 +204,18 @@ def put_file():
             new_id_3dprint = cursor.lastrowid
             conn.commit()
             add_datetime(conn, cursor, new_id_3dprint)
-            response = jsonify(f"{filename} added successfully.")
-            response.status_code = 200
+            response = create_JSON_response(200, f"{filename} added successfully.")
 
-        #response.status_code = 200
+        return response
     except Exception as e:
         traceback.print_exc(file=sys.stderr)
+        return create_JSON_response(500, MSG_500)
     finally:
         if cursor:
             cursor.close()
 
         if conn:
             conn.close()
-
-    return response
 
 
 if __name__ == "__main__":
--- unittests_api_fucntions.py
+++ unittests_api_fucntions.py
@@ -12,34 +12,65 @@ import json
 import unittest
 
 BASE_URL   = "http://127.0.0.1:5000"
-ADD_URL    = BASE_URL + "/file/add"
-FILE_URL   = BASE_URL + "/file/wolf_skull.stl"
-FILES_URL  = BASE_URL + "/files"
-LATEST_URL = BASE_URL + "/file/latestprint"
+BLUE_BOAT  = "blue_boat.stl"
+ADD_URL    = f"{BASE_URL}/file/add"
+FILE_URL   = f"{BASE_URL}/file/{BLUE_BOAT}"
+FILES_URL  = f"{BASE_URL}/files"
+LATEST_URL = f"{BASE_URL}/file/latestprint"
 
-BLUE_BOAT = "blue_boat.stl"
+DEFAULT_DB_HOST = app.app.config['MYSQL_DATABASE_HOST']
+INVALID_DB_HOST = "Intentionally bad hostname"
 
 class TestAPIFuncs(unittest.TestCase):
-    def setUp(self):
+    def _setup_db_host(self, db_host=DEFAULT_DB_HOST):
+        app.app.config['MYSQL_DATABASE_HOST'] = db_host
         self.app = app.app.test_client()
         self.app.testing = True
 
     def test_get_files(self):
+        self._setup_db_host()
         response = self.app.get(FILES_URL)
         data = json.loads(response.get_data())
         self.assertEqual(response.status_code, 200, "Should return HTTP 200.")
 
+    def test_get_files_500(self):
+        self._setup_db_host(INVALID_DB_HOST)
+        response = self.app.get(FILES_URL)
+        data = json.loads(response.get_data())
+        self.assertEqual(response.status_code, 500, "Should return HTTP 500.")
+
     def test_get_file(self):
+        self._setup_db_host()
         response = self.app.get(FILE_URL)
         data = json.loads(response.get_data())
         self.assertEqual(response.status_code, 200, "Should return HTTP 200.")
 
+    def test_get_file_404(self):
+        self._setup_db_host()
+        response = self.app.get(FILE_URL + "does_not_exist")
+        data = json.loads(response.get_data())
+        self.assertEqual(response.status_code, 404, "Should return HTTP 404.")
+
+    def test_get_file_500(self):
+        self._setup_db_host(INVALID_DB_HOST)
+        response = self.app.get(FILE_URL)
+        data = json.loads(response.get_data())
+        self.assertEqual(response.status_code, 500, "Should return HTTP 500.")
+
     def test_get_latest_print(self):
+        self._setup_db_host()
         response = self.app.get(LATEST_URL)
         data = json.loads(response.get_data())
         self.assertEqual(response.status_code, 200, "Should return HTTP 200.")
 
+    def test_get_latest_print_500(self):
+        self._setup_db_host(INVALID_DB_HOST)
+        response = self.app.get(LATEST_URL)
+        data = json.loads(response.get_data())
+        self.assertEqual(response.status_code, 500, "Should return HTTP 500.")
+
     def test_put_add_record(self):
+        self._setup_db_host()
         record = {"filename": BLUE_BOAT, "lastPrintTime": 3650.94738242}
         response = self.app.put(ADD_URL,
                                 data = json.dumps(record),
@@ -47,6 +78,7 @@ class TestAPIFuncs(unittest.TestCase):
         self.assertEqual(response.status_code, 200, "Adding a well formed record should return HTTP 200.")
 
     def test_put_update_record(self):
+        self._setup_db_host()
         record = {"filename": BLUE_BOAT}
         response = self.app.put(ADD_URL,
                                 data = json.dumps(record),
@@ -54,6 +86,7 @@ class TestAPIFuncs(unittest.TestCase):
         self.assertEqual(response.status_code, 200, "Updating a record should return HTTP 200.")
 
     def test_put_no_filename(self):
+        self._setup_db_host()
         record = {}
         response = self.app.put(ADD_URL,
                                 data = json.dumps(record),
@@ -61,11 +94,20 @@ class TestAPIFuncs(unittest.TestCase):
         self.assertEqual(response.status_code, 400, "Adding a record with an empty json payload should return HTTP 400.")
 
     def test_put_error(self):
+        self._setup_db_host()
         record = {"filename": "red_boat.stl"}
         response = self.app.put(ADD_URL,
                                 data = json.dumps(record),
                                 content_type='application/json')
         self.assertEqual(response.status_code, 400, "Adding a record with no \"lastPrintTime\" should return HTTP 400.")
+
+    def test_put_add_record_500(self):
+        self._setup_db_host(INVALID_DB_HOST)
+        record = {"filename": "benchy.stl", "lastPrintTime": 4444.44}
+        response = self.app.put(ADD_URL,
+                                data = json.dumps(record),
+                                content_type='application/json')
+        self.assertEqual(response.status_code, 500, "Should return HTTP 500.")
 
 if __name__ == '__main__':
     unittest.main()
